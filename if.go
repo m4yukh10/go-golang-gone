b8635a13b9edd431ec2aae94b56c099a9270fd63
Mayukh Sen <mayukh.sen120@gmail.com>
2025-10-25 23:46:37 +0530

    Add switch statement example in if.go

---
+++ if.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main(){
+	i := 10;
+	switch i {
+	case 1:
+		fmt.Println("one")
+	case 2:
+		fmt.Println("two")
+	case 3:
+		fmt.Println("three")
+	case 4:
+		fmt.Println("four")
+	default:
+		fmt.Println("we goon!")			
+	}
+
+
+}
